5b56ab4883da32c78959780fba1ab884c05ab57b
lvzongting <lvzongting@gmail.com>
2013-01-25 07:53:34 +0800

    simple the mainlayout

--- gtrans.cpp
+++ gtrans.cpp
@@ -24,136 +24,64 @@
 #include <QNetworkRequest>
 #include <QNetworkReply>
 
-void GTrans::fillInLanguages() {
-    if (languages.size()==0) {
-        languages[tr("French")] = tr("fr");
-        languages[tr("Spanish")] = tr("es");
-        languages[tr("Afrikaans")] = tr("af");
-        languages[tr("Albanian")] = tr("sq");
-                         
-        languages[tr("Arabic")] = tr("ar");
-        languages[tr("Belarusian")] = tr("be");
-        languages[tr("Bulgarian")] = tr("bg");
-        languages[tr("Catalan")] = tr("ca");
-        languages[tr("Chinese")] = tr("zh-cn");
-                         
-        languages[tr("Croatian")] = tr("hr");
-        languages[tr("Czech")] = tr("cs");
-        languages[tr("Danish")] = tr("da");
-        languages[tr("Dutch")] = tr("nl");
-        languages[tr("English")] = tr("en");
-        languages[tr("Estonian")] = tr("et");
-                         
-        languages[tr("Filipino")] = tr("tl");
-        languages[tr("Finnish")] = tr("fi");
-        languages[tr("French")] = tr("fr");
-        languages[tr("Galician")] = tr("gl");
-        languages[tr("German")] = tr("de");
-        languages[tr("Greek")] = tr("el");
-                         
-        languages[tr("Haitian creole alpha")] = tr("ht");
-        languages[tr("Hebrew")] = tr("iw");
-        languages[tr("Hindi")] = tr("hi");
-        languages[tr("Hungarian")] = tr("hu");
-        languages[tr("Icelandic")] = tr("is");
-                         
-        languages[tr("Indonesian")] = tr("id");
-        languages[tr("Irish")] = tr("ga");
-        languages[tr("Italian")] = tr("it");
-        languages[tr("Japanese")] = tr("ja");
-        languages[tr("Korean")] = tr("ko");
-                         
-        languages[tr("Latvian")] = tr("lv");
-        languages[tr("Lithuanian")] = tr("lt");
-        languages[tr("Macedonian")] = tr("mk");
-        languages[tr("Malay")] = tr("ms");
-        languages[tr("Maltese")] = tr("mt");
-                         
-        languages[tr("Norwegian")] = tr("no");
-        languages[tr("Persian")] = tr("fa");
-        languages[tr("Polish")] = tr("pl");
-        languages[tr("Portuguese")] = tr("pt");
-        languages[tr("Romanian")] = tr("ro");
-                         
-        languages[tr("Russian")] = tr("ru");
-        languages[tr("Serbian")] = tr("sr");
-        languages[tr("Slovak")] = tr("sk");
-        languages[tr("Slovenian")] = tr("sl");
-        languages[tr("Spanish")] = tr("es");
-                         
-        languages[tr("Swahili")] = tr("sw");
-        languages[tr("Swedish")] = tr("sv");
-        languages[tr("Thai")] = tr("th");
-        languages[tr("Turkish")] = tr("tr");
-        languages[tr("Ukrainian")] = tr("uk");
-                         
-        languages[tr("Vietnamese")] = tr("vi");
-        languages[tr("Welsh")] = tr("cy");
-        languages[tr("Yiddish")]= tr("yi");
-    }
-    fromLang->addItems(languages.keys());
-    toLang->addItems(languages.keys());
-
-    fromLang->setCurrentIndex(fromLang->findText(tr("English")));
-    toLang->setCurrentIndex(toLang->findText(tr("French")));
-}
-
 GTrans::GTrans() {
-    QLayout *mainLayout = new QVBoxLayout;
 
     //lzt QTextcode modify for Utf8
     QTextCodec::setCodecForTr(QTextCodec::codecForLocale());
     QTextCodec::setCodecForCStrings( QTextCodec::codecForLocale());
+
+    //Layout
+    QLayout *mainLayout = new QVBoxLayout;
+
     // The input section
-    QLayout *top = new QHBoxLayout;
-    QBoxLayout *tl = new QVBoxLayout;
+//    QLayout *top = new QHBoxLayout;
+//    QBoxLayout *tl = new QVBoxLayout;
     QLabel *inLabel = new QLabel(tr("Input:"));
-    fromLang = new QComboBox;
-    tl->addWidget(inLabel);
-    tl->addSpacing(10);
-    tl->addWidget(fromLang);
-    tl->addStretch();
-    top->addItem(tl);
+    mainLayout->addWidget(inLabel);                  //lzt add
+//   fromLang = new QComboBox;
+//    tl->addWidget(inLabel);
+//    tl->addSpacing(10);
+//    tl->addWidget(fromLang);
+//    tl->addStretch();
+//    top->addItem(tl);
     inputTxt = new QTextEdit;
 
-    top->addWidget(inputTxt);
+//    top->addWidget(inputTxt);
+    mainLayout->addWidget(inputTxt);                //lzt add
 
     // The output section
-    QLayout *bottom = new QHBoxLayout;
-    QBoxLayout *bl = new QVBoxLayout;
+//    QLayout *bottom = new QHBoxLayout;
+//    QBoxLayout *bl = new QVBoxLayout;
     QLabel *outLabel = new QLabel(tr("Output:"));
-    toLang = new QComboBox;
-    bl->addWidget(outLabel);
-    bl->addSpacing(10);
-    bl->addWidget(toLang);
-    bl->addStretch();
-    bottom->addItem(bl);
+//    toLang = new QComboBox;
+//    bl->addWidget(outLabel);
+//    bl->addSpacing(10);
+//    bl->addWidget(toLang);
+//    bl->addStretch();
+//    bottom->addItem(bl);
+    mainLayout->addWidget(outLabel);                //lzt add
     outputTxt = new QTextEdit;
     outputTxt->setReadOnly(true);
-    bottom->addWidget(outputTxt);
+//    bottom->addWidget(outputTxt);
+    mainLayout->addWidget(outputTxt);               //lzt add
 
-    mainLayout->addItem(top);
-    mainLayout->addItem(bottom);
+//    mainLayout->addItem(top);
+//    mainLayout->addItem(bottom);
 
     // Translate button
     trans_b = new QPushButton(tr("Translate"));
     mainLayout->addWidget(trans_b);
 
-    fillInLanguages();
-
     setLayout(mainLayout);
     setWindowTitle(tr("Translate"));
-    
+
+
     trans_b->setDefault(true);
     connect(trans_b, SIGNAL(clicked()), this, SLOT(doTrans()));
 
     // Setup foxus and tab order.
     inputTxt->setTabChangesFocus(true);
     inputTxt->setFocus(Qt::ActiveWindowFocusReason);
-    setTabOrder(inputTxt, toLang);
-    setTabOrder(toLang, trans_b);
-    setTabOrder(trans_b, fromLang);
-    setTabOrder(fromLang, inputTxt);
     outputTxt->setFocusProxy(trans_b);
 }
 
