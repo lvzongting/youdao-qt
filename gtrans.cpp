2ebd10f2c0aa9bb46b4b479ec1465e29d5115f3f
lvzongting <lvzongting@gmail.com>
2013-01-25 10:39:09 +0800

    add select from mouse simple the ui

--- gtrans.cpp
+++ gtrans.cpp
@@ -23,6 +23,7 @@
 #include <QUrl>
 #include <QNetworkRequest>
 #include <QNetworkReply>
+#include <QClipboard>
 
 GTrans::GTrans() {
 
@@ -36,30 +37,30 @@ GTrans::GTrans() {
     // The input section
 //    QLayout *top = new QHBoxLayout;
 //    QBoxLayout *tl = new QVBoxLayout;
-    QLabel *inLabel = new QLabel(tr("Input:"));
-    mainLayout->addWidget(inLabel);                  //lzt add
+//    QLabel *inLabel = new QLabel(tr("Input:"));
+//    mainLayout->addWidget(inLabel);                  //lzt add
 //   fromLang = new QComboBox;
 //    tl->addWidget(inLabel);
 //    tl->addSpacing(10);
 //    tl->addWidget(fromLang);
 //    tl->addStretch();
 //    top->addItem(tl);
-    inputTxt = new QTextEdit;
+//    inputTxt = new QTextEdit;
 
 //    top->addWidget(inputTxt);
-    mainLayout->addWidget(inputTxt);                //lzt add
+//    mainLayout->addWidget(inputTxt);                //lzt add
 
     // The output section
 //    QLayout *bottom = new QHBoxLayout;
 //    QBoxLayout *bl = new QVBoxLayout;
-    QLabel *outLabel = new QLabel(tr("Output:"));
+//    QLabel *outLabel = new QLabel(tr("Output:"));
 //    toLang = new QComboBox;
 //    bl->addWidget(outLabel);
 //    bl->addSpacing(10);
 //    bl->addWidget(toLang);
 //    bl->addStretch();
 //    bottom->addItem(bl);
-    mainLayout->addWidget(outLabel);                //lzt add
+//    mainLayout->addWidget(outLabel);                //lzt add
     outputTxt = new QTextEdit;
     outputTxt->setReadOnly(true);
 //    bottom->addWidget(outputTxt);
@@ -69,30 +70,42 @@ GTrans::GTrans() {
 //    mainLayout->addItem(bottom);
 
     // Translate button
-    trans_b = new QPushButton(tr("Translate"));
-    mainLayout->addWidget(trans_b);
+//    trans_b = new QPushButton(tr("Translate"));
+//    mainLayout->addWidget(trans_b);
 
     setLayout(mainLayout);
     setWindowTitle(tr("Translate"));
 
 
-    trans_b->setDefault(true);
-    connect(trans_b, SIGNAL(clicked()), this, SLOT(doTrans()));
+//    trans_b->setDefault(true);
+//    connect(trans_b, SIGNAL(clicked()), this, SLOT(doTrans()));
+
+//lzt clip
+    QClipboard *clipboard = QApplication::clipboard();
+    connect(clipboard, SIGNAL(selectionChanged()), this, SLOT(doTrans()));
+//
 
     // Setup foxus and tab order.
-    inputTxt->setTabChangesFocus(true);
-    inputTxt->setFocus(Qt::ActiveWindowFocusReason);
-    outputTxt->setFocusProxy(trans_b);
+//    inputTxt->setTabChangesFocus(true);
+//    inputTxt->setFocus(Qt::ActiveWindowFocusReason);
+//    outputTxt->setFocusProxy(trans_b);
 }
 
 void GTrans::doTrans() {
     nam = new QNetworkAccessManager(this);
 
     // Figure out the URL for translate.google.com
-    QStringList words = inputTxt->toPlainText().split(QRegExp("\\s+"));
+//  QStringList words = inputTxt->toPlainText().split(QRegExp("\\s+"));
+
+//lzt clipboard
+    QClipboard *clipboard = QApplication::clipboard();
+    QStringList words = clipboard->text(QClipboard::Selection).split(QRegExp("\\s+"));
+//    inputTxt->setPlainText(words.join(" "));
+//
+
     //QString gtUrl = tr("https://translate.google.com/translate_t?langpair=%1|%2&text=").arg(languages[fromLang->currentText()]).arg(languages[toLang->currentText()]) + words.join("+");
     QString gtUrl = tr("http://dict.youdao.com/search?q=") + words.join(" ");
-//  outputTxt->setPlainText(gtUrl);
+//   outputTxt->setPlainText(gtUrl);
 //lzt1
     QUrl url(gtUrl);
 
@@ -109,8 +122,9 @@ void GTrans::doTrans() {
             this, SLOT(finishedSlot(QNetworkReply*)));
 
     // Go back to the text input
-    inputTxt->selectAll();
-    inputTxt->setFocus();
+//    inputTxt->selectAll();
+//    inputTxt->setFocus();
+    outputTxt->setFocus();
 }
 
 void GTrans::finishedSlot(QNetworkReply* reply) {
